0c1cb6e22ab53dc8db277ebd702b801c6f722125
olga <ol4ik_bel89@mail.ru>
2018-12-06 23:04:13 +0300

    added book routes

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,11 +1,21 @@
 'use strict';
 
 const express = require('express');
+
 const controller = require('../controllers/books');
 
 const router = express.Router();
 
-// TODO Add other book routes
-//router.get('/:id', controller.getBookById);
+router.get('/:id', controller.getBookById);
+
+router.get('/', controller.getBooks);
+
+router.put('/:id', controller.validate('updateBook'), controller.updateBook);
+
+router.patch('/:id', controller.validate('patchBook'), controller.patchBook);
+
+router.post('/', controller.validate('createBook'), controller.createBook);
+
+router.delete('/:id', controller.removeBook);
 
 module.exports = router;
