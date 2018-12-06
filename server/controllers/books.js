bf119f8c89042adf58fa748661634d95890d3052
olga <ol4ik_bel89@mail.ru>
2018-12-06 22:55:21 +0300

    added and implemented method "validate". Added function "errorFormatter"

--- server/controllers/books.js
+++ server/controllers/books.js
@@ -1,4 +1,42 @@
 'use strict';
+const Book = require('../dao/book');
+const mongoose = require('mongoose');
+const {body, validationResult} = require('express-validator/check');
+
+const controller = {};
+
+/**
+ * Provide list of validation rules
+ * @param {String} method - the name of controller method
+ * @returns {Array} list of validation rules
+ */
+controller.validate = function (method) {
+    switch (method) {
+        case 'updateBook':
+            return [
+                body('name', 'Name is require').exists({checkFalsy: true})
+            ];
+        case 'createBook':
+            return [
+                body('name', 'Name is require').exists({checkFalsy: true})
+            ];
+        case 'patchBook':
+            return [
+                body ('name', 'Name is require')
+                    .exists({checkFalsy: true})
+                    .optional()
+            ];
+    }
+};
+
+/**
+ * Format validation result (error)
+ * @param {{msg: String}} error - error argument
+ * @returns {String} error text message
+ */
+function errorFormatter({msg}) {
+    return `${msg}`;
+}
 
 /** 
  * Send specific book entity by id
